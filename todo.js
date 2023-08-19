2044a59fb22532bba1c985c835ae2081f13ae142
Leojin18 <124434603+Leojin18@users.noreply.github.com>
2023-08-20 02:32:10 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,25 +1,27 @@
-const inputBox= document.getElementById("input-box")
-const listContainer= document.getElementById("list-container")
-function addTask(){
-    if(inputBox.value === ''){
-        alert(" you must write something");
-    }
-    else{
-        let li=document.createElement("li");
-        li.innerHTML= inputBox.value;
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something");
+    } else {
+        const li = document.createElement("li");
+        li.textContent = inputBox.value;
+        
+        const span = document.createElement("span");
+        span.textContent = "\u00d7";
+        
+        li.appendChild(span);
         listContainer.appendChild(li);
-        let span = document.createElement("span")
-        span.innerHTML= "\u00d7"
-        li.appendChild(span) 
     }
-    inputBox.value=''
-}
-listContainer.addEventListener("click",function(e){
-if(e.target.tagName === "LI"){
-    e.target.classList.toggle("checked")
-}
-else if(e.target.tagName === "SPAN"){
-e.target.parentElement.remove()
+    
+    inputBox.value = '';
 }
 
-},false)
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+}, false)
